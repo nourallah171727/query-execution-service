fda5da69d9fefe758c23754899cd4f3250b0e399
Your Name <gargourinourallah@gmail.com>
2025-11-05 19:53:50 +0100

    added Query entity

---
+++ src/main/java/com/example/demo/entity/Query.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Query {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    public Query() {}
+
+    public Query(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
