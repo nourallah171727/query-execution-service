085ed92469b2afbc2a0a2b279baf46e30ae7b77c
Your Name <gargourinourallah@gmail.com>
2025-11-05 19:57:39 +0100

    added basic get and post functions in querySerice

---
+++ src/main/java/com/example/demo/service/QueryService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Query;
+import com.example.demo.repository.QueryRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QueryService {
+
+    private final QueryRepository repository;
+
+    public QueryService(QueryRepository repository) {
+        this.repository = repository;
+    }
+
+    public Query saveQuery(String text) {
+        return repository.save(new Query(text));
+    }
+
+    public List<Query> getAllQueries() {
+        return repository.findAll();
+    }
+
+
+}
