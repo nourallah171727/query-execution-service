72e75735601c31758ea70bb0e18b66b1d994c7e3
Your Name <gargourinourallah@gmail.com>
2025-11-27 14:46:09 +0100

    added check for unvalid queryId

--- src/main/java/com/example/demo/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exceptionhandler/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.example.demo.exceptionhandler;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 /*this does nothing in the project rn , since we did not provoke any IllegalArgumentException in the main
 request thread , but it's useful to keep maybe when we later add some exceptions
@@ -30,5 +32,13 @@ public class GlobalExceptionHandler {
                         "message", ex.getMessage(),
                         "status", HttpStatus.INTERNAL_SERVER_ERROR.value()
                 ));
+    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(EntityNotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("error", "NOT_FOUND");
+        body.put("message", ex.getMessage());
+        body.put("status", 404);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 }
--- src/main/java/com/example/demo/service/QueryJobSubmitterService.java
+++ src/main/java/com/example/demo/service/QueryJobSubmitterService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Query;
 import com.example.demo.entity.QueryJob;
 import com.example.demo.entity.enums.QueryJobStatus;
 import com.example.demo.repository.QueryJobRepository;
 import com.example.demo.repository.QueryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 @Service
 public class QueryJobSubmitterService {
@@ -26,10 +28,9 @@ public class QueryJobSubmitterService {
     public long submit(long queryId) {
 
         // 1. Load SQL
-        String sql = queryRepo.findSqlById(queryId);
-
+        Query query=queryRepo.findById(queryId).orElseThrow(()->new EntityNotFoundException("no query with such ID exists"));
         // 2. Ask the security service to validate permissions
-        querySecurity.enforcePermission(sql);
+        querySecurity.enforcePermission(query.getText());
 
         // 3. Normal job submission
         QueryJob job = new QueryJob(queryId, QueryJobStatus.RUNNING);
